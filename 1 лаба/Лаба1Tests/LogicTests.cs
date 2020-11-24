2c5eb9006cd44482c8fc7852cf73349f8ce614f7
KomogortsevaYulia <74527737+KomogortsevaYulia@users.noreply.github.com>
2020-11-25 01:51:35 +0800

    Create LogicTests.cs

---
+++ 1 лаба/Лаба1Tests/LogicTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Лаба1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба1.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void GetVariantWordRubleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetVariantWordKopekTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetRezultTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
